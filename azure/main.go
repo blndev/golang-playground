cf197a71abf78d94c4aaeb4cbddadce828b3403d
blndev <blndev@users.noreply.github.com>
2017-11-27 18:36:50 +0100

    implement resource group and status extraction

--- azure/main.go
+++ azure/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strconv" //for parsebool
-
 	"github.com/Azure/azure-sdk-for-go/arm/compute"
 	"github.com/Azure/go-autorest/autorest/azure"
 	"github.com/Azure/go-autorest/autorest/utils"
+	"os"
+	"regexp"  //to extract resourcegroup
+	"strconv" //for parsebool
 )
 
 // Function to analyze a bool Flag value containes in *sring and return true if it is set
@@ -23,18 +23,23 @@ func isFlagged(tagValue *string) bool {
 	return false
 }
 
+var vmClient compute.VirtualMachinesClient
+
 func main() {
 
 	//fmt.Printf("German Cloud: %+v\n", azure.GermanCloud)
 
 	//TODO add ENV Flag to identify GErman or Global Azure CLoud
 	authorizer, _ := utils.GetAuthorizer(azure.GermanCloud)
-	vmClient := compute.NewVirtualMachinesClient(os.Getenv("AZURE_SUBSCRIPTION_ID"))
+	vmClient = compute.NewVirtualMachinesClient(os.Getenv("AZURE_SUBSCRIPTION_ID"))
 	vmClient.Authorizer = authorizer
 	vmClient.BaseURI = azure.GermanCloud.ResourceManagerEndpoint
 
 	//TODO find a way to receive VMs by using a query
 	list, err := vmClient.ListAll()
+	//TODO there is a "nextLink" in the result if the result is paged
+	//see https://godoc.org/github.com/Azure/azure-sdk-for-go/arm/compute#VirtualMachineListResult
+	//use ListAllNExtResults(previousResult)
 	onErrorFail(err, "ListAll failed")
 	if list.Value != nil && len(*list.Value) > 0 {
 		fmt.Println("VMs in subscription")
@@ -69,21 +74,50 @@ func printVM(vm compute.VirtualMachine) {
 	}
 	fmt.Printf("Virtual machine '%s'\n", *vm.Name)
 
+	//find resourcegroup
+	r, _ := regexp.Compile("resourceGroups/(.*)/providers")
+	//r.MatchString(*vm.ID)
+	resoureGroup := r.FindStringSubmatch(*vm.ID)[1]
+	fmt.Println(resoureGroup)
+
 	//see https://godoc.org/github.com/Azure/azure-sdk-for-go/arm/compute#VirtualMachineProperties
+	//statuses seems to be filled only by vmClient.Get(rg,vmName, compute.InstanceView)
 	var vmProps = *vm.VirtualMachineProperties
-	var test *[]compute.InstanceViewStatus
+	var statuses *[]compute.InstanceViewStatus
 	if vmProps.InstanceView != nil {
-		test = (*vmProps.InstanceView).Statuses
+		statuses = (*vmProps.InstanceView).Statuses
 	}
-	if test != nil {
-		for _, status := range *test {
-			fmt.Printf("Status %s, Message %s", status.Code, status.Message)
+
+	if statuses != nil {
+		for _, status := range *statuses {
+			fmt.Printf("Status %s, Message %s", *status.Code, *status.DisplayStatus)
 		}
 	} else {
-		fmt.Println("no status found")
+		fmt.Println("no status found, get detailed vm data")
+		//see https://godoc.org/github.com/Azure/azure-sdk-for-go/arm/compute#VirtualMachinesClient.Get
+		detailedVM, err := vmClient.Get(resoureGroup, *vm.Name, compute.InstanceView)
+		fmt.Println(*detailedVM.Name)
+		//TODO refactor status extraction to separate function
+		if (*detailedVM.VirtualMachineProperties).InstanceView != nil {
+			statuses = (*(*detailedVM.VirtualMachineProperties).InstanceView).Statuses
+		}
+
+		if statuses != nil {
+			for _, status := range *statuses {
+				fmt.Printf("Status %s, Message %s", *status.Code, *status.DisplayStatus)
+			}
+		}
+		if err != nil {
+			//seems not to work, maybe recursion is not possible with go
+			printVM(detailedVM)
+		} else {
+			fmt.Println(err)
+		}
 	}
+
 	elements := map[string]interface{}{
 		"ID":                *vm.ID,
+		"Name":              *vm.Name,
 		"Type":              *vm.Type,
 		"Location":          *vm.Location,
 		"Tags":              tags,
@@ -93,8 +127,8 @@ func printVM(vm compute.VirtualMachine) {
 		"VMSize":            (*vmProps.HardwareProfile).VMSize, //compare with const values from https://godoc.org/github.com/Azure/azure-sdk-for-go/arm/compute#VirtualMachineSizeTypes
 		//"OS-DiskName":       *(*(*vmProps.StorageProfile).OsDisk).Name,
 		//"Instance Status": (*(*vmProps.InstanceView).Statuses),
-
-		"test": test}
+		"ResourceGroup": resoureGroup,
+		"test":          statuses}
 	for k, v := range elements {
 		fmt.Printf("\t%s: %s\n", k, v)
 	}
