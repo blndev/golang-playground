7f5bdd9f3457b4bd35c6e7b23c7d46f6ae972623
blndev <blndev@users.noreply.github.com>
2018-01-13 23:05:52 +0100

    add a help query shortcut

--- console/parameter.go
+++ console/parameter.go
@@ -1,22 +1,24 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "os"
+	"flag"
+	"fmt"
+	"os"
 )
 
 func main() {
-    textPtr := flag.String("text", "", "Text to parse.")
-    metricPtr := flag.String("metric", "chars", "Metric {chars|words|lines};.")
-    debugPtr := flag.Bool("debug", false, "Measure unique values of a metric.")
-    helpPtr := flag.Bool("help", false, "Display this message.")
-    flag.Parse()
-    
-    if *helpPtr {
-        flag.PrintDefaults()
-        os.Exit(0)
-    }
-	
-    fmt.Printf("textPtr: %s, metricPtr: %s, debug: %t\n", *textPtr, *metricPtr, *debugPtr)
+	textPtr := flag.String("text", "", "Text to parse.")
+	metricPtr := flag.String("metric", "chars", "Metric {chars|words|lines};.")
+	debugPtr := flag.Bool("debug", false, "Measure unique values of a metric.")
+	helpPtr := flag.Bool("help", false, "Display this message.")
+	helpSHortPtr := flag.Bool("?", false, "Display this message.")
+
+	flag.Parse()
+
+	if *helpPtr || *helpSHortPtr {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	fmt.Printf("textPtr: %s, metricPtr: %s, debug: %t\n", *textPtr, *metricPtr, *debugPtr)
 }
