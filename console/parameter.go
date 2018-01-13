0c43e125a3457f39726cfdeff05b64cdebb3fbb0
blndev <blndev@users.noreply.github.com>
2018-01-13 22:56:00 +0100

    simpe parameter parsing

---
+++ console/parameter.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+    "os"
+)
+
+func main() {
+    textPtr := flag.String("text", "", "Text to parse.")
+    metricPtr := flag.String("metric", "chars", "Metric {chars|words|lines};.")
+    debugPtr := flag.Bool("debug", false, "Measure unique values of a metric.")
+    helpPtr := flag.Bool("help", false, "Display this message.")
+    flag.Parse()
+    
+    if *helpPtr {
+        flag.PrintDefaults()
+        os.Exit(0)
+    }
+	
+    fmt.Printf("textPtr: %s, metricPtr: %s, debug: %t\n", *textPtr, *metricPtr, *debugPtr)
+}
