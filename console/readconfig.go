7d0990831a249534b6c85c674083bebc5cef012b
blndev <blndev@users.noreply.github.com>
2018-01-15 22:02:12 +0100

    simple server config

--- console/readconfig.go
+++ console/readconfig.go
@@ -9,11 +9,9 @@ import (
 )
 
 type tomlConfig struct {
-	Title string
-	//Owner   ownerInfo
+	Title   string
 	DB      database `toml:"database"`
 	Servers map[string]server
-	//Clients clients
 }
 
 type database struct {
@@ -36,17 +34,11 @@ func main() {
 	}
 
 	fmt.Printf("Title: %s\n", config.Title)
-	/*
-		fmt.Printf("Owner: %s (%s, %s), Born: %s\n",
-		config.Owner.Name, config.Owner.Org, config.Owner.Bio,
-		config.Owner.DOB)
-	*/
+
 	fmt.Printf("Database: %s %v (Max conn. %d), Enabled? %v\n",
 		config.DB.Server, config.DB.Ports, config.DB.ConnMax,
 		config.DB.Enabled)
 	for serverName, server := range config.Servers {
 		fmt.Printf("Server: %s (%s, %s)\n", serverName, server.IP, server.DC)
 	}
-	//fmt.Printf("Client data: %v\n", config.Clients.Data)
-	//fmt.Printf("Client hosts: %v\n", config.Clients.Hosts)
 }
